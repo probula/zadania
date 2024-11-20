cdabe210a1b847a55671b9188f9eebca5fcdcb73
igor <igor@com.pl>
2024-11-20 14:30:47 +0100

    first

---
+++ SimpleTextProcessor.js
@@ -0,0 +1,21 @@
+const processText = (text, {uppercase = false, reverse = false, removeSpecialChars = false } = {}) =>{
+    let tekst = text;
+    if(uppercase){
+        tekst = text.toUpperCase()
+        // console.log(`wyraz z duzych liter: ${tekst}`)
+    }
+    if(reverse){
+        tekst = [...tekst].reverse().join("") //rozdziela na litery, odwraca, łączy i usuwa przecinki
+        //console.log(`wyraz w odwrotnej kolejnosci: ${tekst2}`)
+    }
+    if(removeSpecialChars){
+        tekst = tekst.replace(/[\W_]/g, ""); //zamienia wszystkie znaki specjalne na puste ""
+        //console.log(`wyraz z usunietymi znakami specjalnymi ${tekst}`)
+    }
+    console.log(`wyraz: ${tekst}`)
+}
+const text = "tE$StOWY&&"
+
+processText(text, {uppercase: false, reverse: true, removeSpecialChars: true})
+
+
